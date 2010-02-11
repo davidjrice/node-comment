12992f8a4b53efc3c2439304786d9edbc3ce78c6
David Rice <me@davidjrice.co.uk>
2010-02-11 00:06:25 +0000

    Tidy up test

--- test/test_mustache.js
+++ test/test_mustache.js
@@ -1,12 +1,9 @@
 process.mixin(require("./common"));
-
 var Mustache = require('../dep/node-mustache')
 
 var got_error = false;
-
 var filename = path.join(fixturesDir, "test_mustache_with_no_template_tags.html");
 var promise = posix.cat(filename, "raw");
-
 var html_fixture = "<html><head></head><body><h1>Test</h1></body></html>"
 
 promise.addCallback(function (template) {
